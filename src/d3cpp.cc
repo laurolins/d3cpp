b1feb267f746adf481fd3d0aae065f7c9afb2627
Lauro Lins <lauro.lins@gmail.com>
2015-04-06 19:11:38 -0400

    exit selection first test

--- src/d3cpp.cc
+++ src/d3cpp.cc
@@ -13,14 +13,16 @@
 struct Element {
 public:
     Element() = default;
-    Element(const std::string& tag, Element* parent=nullptr);
+    Element(const std::string& tag, Element* parent=nullptr, int parent_index=0);
     Element& append(const std::string &tag);
     Element& attr(const std::string& key, const std::string& value);
     const std::string& attr(const std::string &key);
+    void remove();
 public:
     std::string tag;
     Element*    parent {nullptr};
-    std::vector<std::unique_ptr<Element>> children;
+    int         parent_index;
+    std::vector<std::unique_ptr<Element>> children; // might have nullptrs inside
     std::map<std::string, std::string> attributes;
 };
 
@@ -77,8 +79,8 @@ struct Group {
     Group(Element* parent_node);
     Group(Element* parent_node, Element* single);
     
-    Group& append(Element* e, T value);
-    Group& append(Element* e);
+    Group& add(Element* e, T value);
+    Group& add(Element* e);
     
     Element* parent_node;
     std::vector<element_value_type> elements;
@@ -88,15 +90,22 @@ struct Group {
 template <typename T>
 struct EnterSelection;
 
+template <typename T>
+struct ExitSelection;
+
 //------------------------------------------------------------------------------
 // Selection
 //------------------------------------------------------------------------------
+
+// two types are associated with a selection: the enter/update type
+// and the exit type
 
 template <typename T>
 struct Selection {
 public:
 
-    using group_type = Group<T>;
+    using selection_type = Selection;
+    using group_type     = Group<T>;
     
 public:
     
@@ -113,15 +122,22 @@ public:
     Selection append(const std::string& tag);
     
     EnterSelection<T>& enter();
+    ExitSelection<T>&  exit();
     
     Selection<T> selectAll(const std::string &tag);
     
 public:
     void _enterSelection_init(const std::vector<T>& extra_data);
     void _enterSelection_add(group_type* main_selection_parent_children, int index);
+
+public:
+    void _exitSelection_init();
+    void _exitSelection_add(std::function<void()> remove_function);
+
 public:
     std::vector<std::unique_ptr<group_type>> groups;
     std::unique_ptr<EnterSelection<T>> enter_selection;
+    std::unique_ptr<ExitSelection<T>> exit_selection;
 };
 
 
@@ -168,30 +184,63 @@ struct EnterSelection {
     // let's do by index first
 };
 
+//------------------------------------------------------------------------------
+// ExitSelection
+//------------------------------------------------------------------------------
+
+template <typename T>
+struct ExitSelection {
+    
+    using remove_function_type = std::function<void()>;
+    
+    using group_type = Group<T>;
+    
+    struct Entry {
+        Entry() = default;
+        Entry(remove_function_type remove_function);
+        remove_function_type  remove_function;
+    };
+    
+    ExitSelection(Selection<T> *update_selection);
+    
+    ExitSelection& add(remove_function_type f);
+    
+    void remove();
+
+    Selection<T> *update_selection;
+    std::vector<Entry> entries;
+    
+    // let's do by index first
+};
 
 
+//--------------------------------------------------------------------------------------
+// ExitSelection Impl.
+//--------------------------------------------------------------------------------------
 
 
+template <typename T>
+ExitSelection<T>::Entry::Entry(remove_function_type remove_function):
+    remove_function(remove_function)
+{}
 
+template <typename T>
+ExitSelection<T>::ExitSelection(Selection<T> *update_selection):
+    update_selection(update_selection)
+{}
 
+template <typename T>
+auto ExitSelection<T>::add(remove_function_type remove_function) -> ExitSelection& {
+    entries.push_back({remove_function});
+    return *this;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+template <typename T>
+void ExitSelection<T>::remove() {
+    for (auto &e: entries) {
+        e.remove_function();
+    }
+}
 
 
 //------------------------------------------------------------------------------
@@ -212,7 +261,7 @@ Selection<int> Document::selectAll(const std::string &tag) {
     ElementIterator it(root);
     while (auto e=it.next()) {
         if (e->tag.compare(tag) == 0) {
-            group.append(e);
+            group.add(e);
         }
     }
     return result;
@@ -223,7 +272,7 @@ Selection<int> Document::selectAll(const std::string &tag) {
 //------------------------------------------------------------------------------
 
 template <typename T>
-EnterSelection<T>::Entry::Entry(group_type* parent_children, int index):
+EnterSelection<T>::Entry::Entry(group_type* group, int index):
 group(group),
 index(index)
 {}
@@ -252,8 +301,8 @@ Selection<T> EnterSelection<T>::append(const std::string& tag) {
         auto &new_group = result._group_add(e.group->parent_node);
         for (auto it=enter_data.begin() + e.index;it!=enter_data.end();++it) {
             auto &new_element = e.group->parent_node->append(tag);
-            new_group.append(&new_element, *it);
-            e.group->append(&new_element, *it);
+            new_group.add(&new_element, *it);
+            e.group->add(&new_element, *it);
         }
     }
     return result;
@@ -291,13 +340,13 @@ Group<T>::Group(Element* parent_node, Element* single):
 }
 
 template<typename T>
-auto Group<T>::append(Element* e, T value) -> Group<T>& {
+auto Group<T>::add(Element* e, T value) -> Group<T>& {
     elements.push_back({e,value});
     return *this;
 }
 
 template<typename T>
-auto Group<T>::append(Element* e) -> Group<T>& {
+auto Group<T>::add(Element* e) -> Group<T>& {
     elements.push_back({e});
     return *this;
 }
@@ -344,6 +393,7 @@ Selection<U> Selection<T>::data(const std::vector<U>& data) {
     // match by index
     
     result._enterSelection_init(data);
+    result._exitSelection_init();
     
     for (auto &g: groups) {
         
@@ -353,7 +403,7 @@ Selection<U> Selection<T>::data(const std::vector<U>& data) {
         auto it_ev    = g->elements.begin();
         auto index = 0;
         for (;it_data!= data.end() && it_ev!=g->elements.end();++it_data,++it_ev) {
-            new_group.append(it_ev->element, *it_data);
+            new_group.add(it_ev->element, *it_data);
             ++index;
         }
 
@@ -361,10 +411,18 @@ Selection<U> Selection<T>::data(const std::vector<U>& data) {
         // this is still wrong
         //
         
-        if (it_data != data.end()) {
-            result._enterSelection_add(&new_group,index);
-        }
-  
+        result._enterSelection_add(&new_group,index);
+        
+        auto original_group = g.get();
+        
+        result._exitSelection_add([index,original_group]() {
+            auto a = original_group->elements.begin()+index;
+            auto b = original_group->elements.end();
+            for (auto it=a; it!=b; ++it) {
+                it->element->remove();
+            }
+            original_group->elements.erase(a,b);
+        });
         //
         // if (it_ev != e->elements.end()) {
         //    result.setExitSelection(std::vector<U>(it_ev,))
@@ -400,10 +458,27 @@ void Selection<T>::_enterSelection_add(group_type *main_selection_parent_childre
     enter_selection->add(main_selection_parent_children,index);
 }
 
+template<typename T>
+void Selection<T>::_exitSelection_init() {
+    exit_selection.reset(new ExitSelection<T>(this));
+}
+
+template<typename T>
+void Selection<T>::_exitSelection_add(std::function<void()> remove_function) {
+    if (!enter_selection)
+        throw std::runtime_error("ooops");
+    exit_selection->add(remove_function);
+}
+
 
 template<typename T>
 EnterSelection<T>& Selection<T>::enter() {
     return *enter_selection.get();
+}
+
+template<typename T>
+ExitSelection<T>& Selection<T>::exit() {
+    return *exit_selection.get();
 }
 
 template<typename T>
@@ -415,7 +490,7 @@ Selection<T> Selection<T>::selectAll(const std::string &tag) {
             group_type& g = result._group_add(ev.element);
             while (auto e = it.next()) {
                 if (e->tag.compare(tag) == 0) {
-                    g.append(e);
+                    g.add(e);
                 }
             }
         }
@@ -427,11 +502,10 @@ template<typename T>
 std::ostream& operator<<(std::ostream &os, const Selection<T>& sel) {
     os << "[selection]" << std::endl;
     for (auto &g: sel.groups) {
-        
         os << "    [group]" << std::endl;
-        os << "        [parent_node] <" << g->parent_node->tag << ">" << std::endl;
+        os << "        [parent_node]  <" << g->parent_node->tag << ">" << std::endl;
         for (auto &ev: g->elements) {
-            os << "            [element] " << ev.element->tag << ">" << std::endl;
+            os << "            [element]  <" << ev.element->tag << ">" << std::endl;
         }
     }
     return os;
@@ -443,10 +517,15 @@ std::ostream& operator<<(std::ostream &os, const Selection<T>& sel) {
 // Element Impl.
 //------------------------------------------------------------------------------
 
-Element::Element(const std::string& tag, Element* parent):
+Element::Element(const std::string& tag, Element* parent, int parent_index):
 tag(tag),
-parent(parent)
+parent(parent),
+parent_index(parent_index)
 {}
+
+void Element::remove() {
+    parent->children[parent_index].reset();
+}
 
 Element& Element::append(const std::string &tag) {
     children.push_back(std::unique_ptr<Element>(new Element(tag,this)));
@@ -483,7 +562,8 @@ std::ostream& operator<<(std::ostream &os, const Element& e) {
             }
             os << ">" << std::endl;
             for (auto &c: e.children) {
-                print(*c.get(), level + 1);
+                if (c)
+                    print(*c.get(), level + 1);
             }
             os << prefix << "</" << e.tag << ">" << std::endl;
         }
@@ -527,9 +607,10 @@ Element* ElementIterator::next() {
     stack.pop_front();
     
     // schedule processing of childrens
-    if (max_depth != UNBOUNDED && item.depth < max_depth) {
+    if (max_depth == UNBOUNDED || item.depth < max_depth) {
         for (auto it=item.element->children.rbegin();it!=item.element->children.rend();++it) {
-            stack.push_back( {it->get(), item.depth+1} );
+            if (it->get())
+                stack.push_back( {it->get(), item.depth+1} );
         }
     }
     
@@ -564,24 +645,29 @@ int main() {
 
     auto update_sel = document.selectAll("a").data(points);
 
-    std::cout << document.selectAll("a").data(points);
 
     update_sel.enter().append("a")
        .attr("x",[](const Point& p, int i) { return std::to_string(p.x); })
        .attr("y",[](const Point& p, int i) { return std::to_string(p.y); });
 
+    
+    std::cout << document.selectAll("a");
+
     update_sel
     .attr("x",[](const Point& p, int i) { return std::to_string(p.y); })
     .attr("y",[](const Point& p, int i) { return std::to_string(p.x); });
     
+    std::cout << update_sel;
     std::cout << document.selectAll("a");
 
     document.selectAll("a").selectAll("b").data(points).enter().append("b");
     
-//    data(points).enter().append("b");
+    std::cout << document.selectAll("a").selectAll("b").data(points).attr("z",[](const Point& p, int i) { return std::to_string(p.y); });
 
-    
-    
+    std::cout << root;
+
+    std::vector<int> numbers { 12,13 };
+    document.selectAll("a").selectAll("b").data(numbers).exit().remove();
     
     std::cout << root;
     
